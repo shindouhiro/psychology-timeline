87a44ab52ea56db1e262b36f5ce87f949fd733f6
Hikaru <shindouhiro@gmail.com>
2026-05-11 10:22:16 +0800

    ci: add github actions for pages deployment

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -1,7 +1,20 @@
 import type { NextConfig } from "next";
 
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
+
+let basePath = '';
+
+if (isGithubActions && process.env.GITHUB_REPOSITORY) {
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '');
+  basePath = `/${repo}`;
+}
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  basePath: basePath,
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
